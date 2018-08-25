cc61c159f134817826fe55caf727d693b2a377c0
Azizou Ogbone <ogboneazizou@gmail.com>
2018-08-25 02:25:40 +0200

    add gulpfile. Need browser to test sprite since these have images

--- spec/gameObjects/spriteSpec.js
+++ spec/gameObjects/spriteSpec.js
@@ -16,49 +16,4 @@ describe('Sprite', function () {
     expect(sprite.centerX).toBe(1.5);
     expect(sprite.centerY).toBe(1.5);
   });
-
-  describe('Rectangle#overlap', function () {
-    var rect1;
-    var rect2;
-    var onOverlapCallback;
-    it('should have the method overlap', function () {
-      rect1 = Rectangle.ZERO;
-      expect(rect1.overlap).toBeDefined();
-      expect(typeof rect1.overlap).toBe('function');
-    });
-
-    it('should return true when 2 rectangles overlap', function () {
-      rect1 = new Rectangle(0, 0, 1, 1);
-      rect2 = new Rectangle(0.5, 0.5, 1.5, 1.5);
-      expect(rect1.overlap(rect2, undefined)).toBe(true);
-    });
-
-    it('should return false when 2 rectangles do not overlap', function () {
-      rect1 = new Rectangle(0, 0, 1, 1);
-      rect2 = new Rectangle(1.01, 1, 1.5, 1.5);
-      expect(rect1.overlap(rect2, undefined)).toBe(false);
-    });
-
-    it('should return true when 2 rectangles touches', function () {
-      rect1 = new Rectangle(0, 0, 1, 1);
-      rect2 = new Rectangle(1, 1, 1.5, 1.5);
-      expect(rect1.overlap(rect2, undefined)).toBe(true);
-    });
-
-    it('should invoke the callback when 2 rectangles overlap', function () {
-      rect1 = new Rectangle(0, 0, 1, 1);
-      rect2 = new Rectangle(0.5, 0.5, 1.5, 1.5);
-      onOverlapCallback = jasmine.createSpy('onOverlapCallback');
-      expect(rect1.overlap(rect2, onOverlapCallback)).toBe(true);
-      expect(onOverlapCallback).toHaveBeenCalled();
-    });
-
-    it('should not invoke the callback when 2 rectangles do not overlap', function () {
-      rect1 = new Rectangle(0, 0, 1, 1);
-      rect2 = new Rectangle(1.01, 1, 1.5, 1.5);
-      onOverlapCallback = jasmine.createSpy('onOverlapCallback');
-      expect(rect1.overlap(rect2, onOverlapCallback)).toBe(false);
-      expect(onOverlapCallback).not.toHaveBeenCalled();
-    });
-  });
 });
