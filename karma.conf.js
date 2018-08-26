14bf37b0ee03e7e5b8b6aa736d4d5e23caad2eda
Azizou Ogbone <ogboneazizou@gmail.com>
2018-08-26 04:09:17 +0200

    add browser test with karma and coverage with istanbul

---
+++ karma.conf.js
@@ -0,0 +1,94 @@
+// Karma configuration
+// Generated on Sat Aug 25 2018 21:34:53 GMT+0200 (South Africa Standard Time)
+var istanbul = require('browserify-istanbul');
+var puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/**/*.js',
+      'spec/**/*.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+      'src/test.js',
+      'src/index.js',
+      'src/gameObjects/index.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*.js': ['browserify'],
+      'spec/**/*.js': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        istanbul({
+          instrumenterConfig: {
+            embedSource: true
+          },
+          ignore: ['**/node_modules', '**/spec/**/*.js']
+        })
+      ]
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    // coverageReporter: {
+    //   type: 'text-summary'
+    // },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+    // browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  });
+};
